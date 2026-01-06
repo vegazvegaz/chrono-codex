663fbf82f70996d4bf846fca0fe1c90474c3f7ab
vegazvegaz <julien@vegazvegaz.com>
2026-01-06 10:39:34 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,3 @@
+const gamesData = [
+    { "name": "CrossCode", "sH": 45, "pH": 0, "img": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/368340/library_600x900.jpg", "last": 1736100000, "color": "#45b1e8", "comp": 15, "desc": "Design V136 restauré. En attente du robot..." }
+];
